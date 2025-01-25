f71b68949caa57e8ca56e8a2710d4d3426924e8e
sandeshcs <sandeshcs2921@gmail.com>
2025-01-25 17:03:57 +0530

    book api includes delete api, all books api

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import {useState} from 'react';
+import AllBooks from "./components/AllBooks";
+
+function App() {
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    publishedYear: "",
+    genre: "",
+    language: "",
+    country: "",
+    rating: "",
+    summary: "",
+    coverImageUrl: ""
+  });
+  
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prevData) => ({
+     ...prevData, [name]: name === "publishedYear" || name === "rating"?parseInt(value):value 
+    }));
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try{
+      const response = await fetch('http://localhost:3000/books', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+      console.log(response);
+      if(!response.ok){
+        console.log("failed to add book.");
+      }
+      const data = await response.json();
+      console.log('added book: ', data);
+    }
+    catch (error) {
+      throw error;
+    }
+  }
+  return (
+    <div className="container">
+      <h1>Add New Book</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Title:</label><br/>
+        <input type='text' name="title" value={formData.title} onChange={handleChange}/><br/><br/>
+
+        <label>Author:</label><br/>
+        <input type='text' name="author" value={formData.author} onChange={handleChange}/><br/><br/>
+
+        <label>Published Year:</label><br/>
+        <input type='number' name="publishedYear" value={formData.publishedYear} onChange={handleChange}/><br/><br/>
+
+        <label>Genre:</label><br/>
+        <input type='text' name="genre" value={formData.genre} onChange={handleChange}/><br/><br/>
+
+        <label>Language:</label><br/>
+        <input type='text' name="language" value={formData.language} onChange={handleChange}/><br/><br/>
+
+        <label>Rating:</label><br/>
+        <input type='number' name="rating" value={formData.rating} onChange={handleChange}/><br/><br/>
+        
+        <label>Country:</label><br/>
+        <input type='text' name="country" value={formData.country} onChange={handleChange}/><br/><br/>
+
+        <label>summary:</label><br/>
+        <input type='text' name="summary" value={formData.summary} onChange={handleChange}/><br/><br/>
+        
+        <label>Cover Image Url:</label><br/>
+        <input type='text' name="coverImageUrl" value={formData.coverImageUrl} onChange={handleChange}/><br/><br/>
+
+        <button type="submit">submit</button>
+      </form>
+      <h2>All Book Title List</h2>
+      <AllBooks/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AllBooks.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import useFetch from "../useFetch";
+
+const AllBooks = () => {
+    const [successMessage, setMessage] = useState('');
+    const {data, loading, error} = useFetch('http://localhost:3000/allbooks');
+    //console.log(data);
+
+    const handleDelete = async (bookId) => {
+        try{
+            const response = await fetch(`http://localhost:3000/books/delete/${bookId}`, {
+                method: 'DELETE',
+                headers: {
+                    'content-type':'application/json'
+                },
+            });
+
+            if(!response.ok){
+                throw 'failed to delete book';
+            }
+            const data = await response.json();
+            if(data){
+                setMessage("book deleted successfully.")
+                window.location.reload();
+            }
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
+    return(
+        <div className="container">
+            {loading && <p>loading...</p>}
+            <ul>
+                {data? data.map(book => (
+                    <li key={book._id}>
+                        {book.title}
+                        <button onClick={() => handleDelete(book._id)}>Delete</button>
+                    </li>
+                )): error && <p>{error}</p>}
+            </ul>
+            <p>{successMessage}</p>
+        </div>
+    )
+}
+export default AllBooks;
